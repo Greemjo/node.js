bb677973e379964b906dac0a226b21d317dffac2
bunyod80 <bunyod80@gmail.com>
2018-04-08 13:26:42 +0500

    Fix airport DAO and update test.js

--- dao/AirportDao.js
+++ dao/AirportDao.js
@@ -1,43 +1,38 @@
-const dao = require('./base');
-const Airport = require('../models/Airport');
+const dao = require("./base");
+const Airport = require("../models/Airport");
 
+class AirportDao {
+  static async add(airport) {
+    return await dao.knex.insert(airport).from("Aerport");
+  }
 
-class AirportDao  {
-    static async add(airport){
-        return await dao.knex
-        insert(airport)
-            .from('Aerport')
-    }
+  static async update(id, airport) {
+    return await dao.knex
+      .update({ name, country, region, img_url })
+      .from("Aerport")
+      .where({ id });
+  }
 
-    static async update(id, airport){
-        return await dao.knex
-        update({name,country, region, img_url})
-            .from('Aerport')
-            .where({id})
-    }
+  static async getById(id) {
+    const data = await dao.knex
+      .select()
+      .from("Aerport")
+      .where({ id })
+      .first();
+    return new Airport(data);
+  }
 
-    static async getById(id) {
-        const data = await dao.knex
-            .select()
-            .from('Aerport')
-            .where({ id })
-            .first();
-        return new Airport(data);
-    }
+  static async getList() {
+    const airport_arr = await dao.knex.select().from("Aerport");
+    return airport_arr.map(airport => new Airport(airport));
+  }
 
-    static async getList() {
-        const airport_arr = await dao.knex
-            .select()
-            .from('Aerport');
-        return airport_arr.map(airport => new Airport(airport));
-    }
-
-    static async delete(id) {
-        return dao.knex
-            .from('Aerport')
-            .where({ id })
-            .del();
-    }
+  static async delete(id) {
+    return dao.knex
+      .from("Aerport")
+      .where({ id })
+      .del();
+  }
 }
 
 module.exports = AirportDao;
