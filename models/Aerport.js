245f8a12351a994476ce49a2275d56b611a69b20
anorboev <anorboyev@gmail.com>
2018-04-08 12:23:21 +0500

    deleted files

--- models/Aerport.js
+++
@@ -1,9 +0,0 @@
-class Aerport {
-    constructor({id,name,country,region,img_url}){
-        this.id = id;
-        this.name = name;
-        this.country = country;
-        this.region = region;
-    }
-}
-module.exports = Aerport;
