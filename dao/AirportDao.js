95b41ea75a73a2b88d761a60ddc69ac746f54c4a
mrbekuwl@gmail.com <27bekki>
2018-04-08 12:22:14 +0500

    AirportDao has been created

--- dao/AirportDao.js
+++ dao/AirportDao.js
@@ -1,16 +1,6 @@
 const dao = require('./base');
+const Airport = require('../models/Airport');
 
-
-class Airport {
-
-    constructor({id,name, country, region, img_url}){
-        this.id = id;
-        this.name = name;
-        this.country = country;
-        this.region = region;
-        this.img_url = img_url;
-    }
-}
 
 class AirportDao  {
     static async addAirport({id, name, country, region, img_url}){
---
+++ models/Airport.js
@@ -0,0 +1,13 @@
+class Airport {
+
+    constructor({id,name, country, region, img_url}){
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.region = region;
+        this.img_url = img_url;
+    }
+}
+
+
+module.exports = Airport;
