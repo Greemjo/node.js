1b661f818f4aa4abf11fa09f6101309a6f3e98d1
anorboev <anorboyev@gmail.com>
2018-04-08 12:08:08 +0500

    coolumn name changed

---
+++ models/Aerport.js
@@ -0,0 +1,9 @@
+class Aerport {
+    constructor({id,name,country,region,img_url}){
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.region = region;
+    }
+}
+module.exports = Aerport;
